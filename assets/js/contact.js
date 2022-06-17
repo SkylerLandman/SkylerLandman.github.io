16ceb777423ad5afc65044d0821cfcec777d3230
SkylerLandman <106682564+SkylerLandman@users.noreply.github.com>
2022-06-17 15:09:29 +0200

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,20 @@
+function conctact() {
+    var script_url ="https://script.google.com/macros/s/AKfycbzpdLIPOCokp3pGiZVfIYk3thKh132E5ztJEb-SU2KIzbQCviDdlRGrG-YWaNtZ5s6c/exec";
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+
+    var url = script_url + "?callback=thanks&name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&subject=" + encodeURIComponent(subject) + "&message=" + encodeURIComponent(message);
+    jQuery.ajax({
+        crossDomain: true,
+        url: url,
+        method: "GET",
+        dataType: "jsonp"
+    }) 
+}
+
+function thanks(e) {
+    alert("Thanks, I'll get back to you as soon as I can. <3 ");
+}
